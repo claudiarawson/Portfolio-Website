0abc945d49264c727f590666baf05c339eb5b63c
claudiarawson <claudiarawson2021@gmail.com>
2025-03-27 16:41:35 -0700

    Footer Stylistic Changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,37 +7,37 @@ const Footer  = () => {
     return (
 
         <footer className="body footer container wrapper">
-            <div class="container">
-                <div class="content__footer">
-                    <div class="profil">
+            <div className="container">
+                <div className="content__footer">
+                    <div className="profil">
                         <div className="logo__area">
                             <img src={logo} alt="logo" className="logo"/>
-                            <spam class="logo__name">Claudia's World</spam>
+                            <spam className="logo__name">Claudia's World</spam>
                         </div>
-                        <div class="disc__area">
+                        <div className="disc__area">
                             <p>Thank you for visiting! If you like my work, please check out my other social media:)</p>
                         </div>
-                        <div class="social__media">
-                            <a href="#"><i class='bx bxl-linkedin-square'></i></a>
-                            <a href="#"><i class='bx bxl-github'></i></a>
-                            <a href="#"><i class='bx bx-envelope'></i></a>
-                            <a href="#"><i class='bx bxl-facebook-square' ></i></a>
+                        <div className="social__media">
+                            <a href="#"><i className='bx bxl-linkedin-square'></i></a>
+                            <a href="#"><i className='bx bxl-github'></i></a>
+                            <a href="#"><i className='bx bx-envelope'></i></a>
+                            <a href="#"><i className='bx bxl-facebook-square' ></i></a>
                         </div>
                     </div>
-                    <div class="service__area">
-                        <ul class="service__header">
+                    <div className="service__area">
+                        <ul className="service__header">
                             <li className="service__name">Resources</li>
                             <li><a href="#">Graphics</a></li>
                             <li><a href="#">Github Repo</a></li>
                             <li><a href="#">Other Resources</a></li>
                         </ul>
-                        <ul class="service__header">
+                        <ul className="service__header">
                             <li className="service__name">Academic Persuit</li>
                             <li><a href="#">Publications</a></li>
                             <li><a href="#">Academic Projects</a></li>
                             <li><a href="#">School Homework</a></li>
                         </ul>
-                        <ul class="service__header">
+                        <ul className="service__header">
                             <li className="service__name">My Hobbies</li>
                             <li><a href="#">Blog</a></li>
                             <li><a href="#">Photography</a></li>
@@ -46,8 +46,8 @@ const Footer  = () => {
                     </div>
                 </div>
             </div>
-            
-            <p>&copy; {new Date().getFullYear()} My Website. All rights reserved.</p>
+            <hr className="custom-hr"/>
+            <div className="footer__bottom">&copy; {new Date().getFullYear()} My Website. All rights reserved.</div>
         </footer>
     );
 };
