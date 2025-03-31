cf37bcfc63a531f6bff291b0cd0080f94ff7d927
claudiarawson <claudiarawson2021@gmail.com>
2025-03-31 14:23:25 -0700

    Header Styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,14 +4,18 @@ import "../styles/Header.css";
 
 const Header = () => {
     return (
-        <header className="header sticky">
+        <header className="header sticky glass">
             <nav>
                 <ul className="nav-list">
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/projects">Projects</Link></li>
-                    <li><Link to="/contact">Contact Me</Link></li>
-                    <li><Link to="/about">About</Link></li>
-                    <li><Link to="/playground">Playground:)</Link></li>
+                    <li className="nav-item">
+                        <Link to="/">
+                            <button className="nav-button">Home</button>
+                        </Link>
+                    </li>
+                    <li className="nav-item"><Link to="/projects">Projects</Link></li>
+                    <li className="nav-item"><Link to="/contact">Contact Me</Link></li>
+                    <li className="nav-item"><Link to="/about">About</Link></li>
+                    <li className="nav-item"><Link to="/playground">Playground :)</Link></li>
                 </ul>
             </nav>
         </header>
