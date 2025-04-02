870a29f43f36f9ce84f619f89cb3ee849c207b11
claudiarawson <claudiarawson2021@gmail.com>
2025-04-01 17:18:43 -0700

    Contact Page Styling

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,10 +1,17 @@
 import React from "react";
+import "../styles/Contact.css";
 
 const Contacts = () => {
     return (
-        <div>
-            <h2>Contacts Page</h2>
-            <p>This is a placeholder for your contacts.</p>
+        <div className="container">
+            <div className="text floated">
+                <h1>Let's Connect!</h1>
+                <h3>I would love to hear from you.</h3>
+                <p>Got a question, a project idea, or just want to say hello? Feel free to reach out! Whether it's about collaboration, coding, or just a friendly chat, I'm always open to connecting. Drop me a message through the form below, or find me directly through one of these social media links. I'll do my best to get back to you as soon as possible!</p>
+            </div>
+            <div className="image">
+                <img src="src/assets/img/headshot.JPG" alt="Head Shot"/>
+            </div>
         </div>
     );
 };
